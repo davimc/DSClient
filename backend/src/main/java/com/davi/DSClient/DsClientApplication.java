39f699136eb505bc1efd6d9b208c2ef177e8edc8
davimc <davimatosc@hotmail.com>
2022-06-12 23:03:01 -0300

    first commit

---
+++ backend/src/main/java/com/davi/DSClient/DsClientApplication.java
@@ -0,0 +1,13 @@
+package com.davi.DSClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsClientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/davi/DSClient/DsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.DSClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
