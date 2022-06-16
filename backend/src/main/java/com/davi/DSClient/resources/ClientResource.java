b4efd9ecc3c617fff616aa62d7176be10cf17eba
davimc <davimatosc@hotmail.com>
2022-06-16 03:50:52 -0300

    Correcting findAllPaged: size to linesPerPage

--- backend/src/main/java/com/davi/DSClient/resources/ClientResource.java
+++ backend/src/main/java/com/davi/DSClient/resources/ClientResource.java
@@ -23,11 +23,11 @@ public class ClientResource {
     @GetMapping
     public ResponseEntity<Page<ClientDTO>> findAllPaged(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "size", defaultValue = "12") Integer size,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
             @RequestParam(value = "direction", defaultValue = "DESC") String direction,
             @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
     ){
-        PageRequest pageRequest = PageRequest.of(page,size, Sort.Direction.valueOf(direction),orderBy);
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction),orderBy);
         return ResponseEntity.ok().body(service.findAllPaged(pageRequest));
     }
     @GetMapping(value = "/{id}")
